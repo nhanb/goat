1f72648eb37fdb9d07d94c98f7f3b8068b73c8d8
Bui Thanh Nhan <nhan.bui@eastagile.com>
2016-05-20 08:45:54 +0700

    Model for list Items and associated migration

--- lists/tests.py
+++ lists/tests.py
@@ -4,6 +4,7 @@ from django.http import HttpRequest
 from django.template.loader import render_to_string
 
 from lists.views import home_page
+from lists.models import Item
 
 
 class HomePageTest(TestCase):
@@ -33,6 +34,20 @@ class HomePageTest(TestCase):
 
 
 class ItemModelTest(TestCase):
-    # TODO
-    # http://chimera.labs.oreilly.com/books/1234000000754/ch05.html#_the_django_orm_and_our_first_model
-    pass
+
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = 'The first (ever) list item'
+        first_item.save()
+
+        first_item = Item()
+        first_item.text = 'Item the second'
+        first_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(second_saved_item.text, 'Item the second')
